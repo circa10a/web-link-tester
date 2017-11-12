3efbb5f63b352e652488268a2e74bee6c5be05da
circa10a <caleblemoine@gmail.comS>
2017-11-11 22:18:14 -0600

    add noty, fixed JS validation

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,12 +1,19 @@
-function loader(){
-  $('.overlay').show();
-  $('.loader').show();
-}
-
 function submit() {
   formVal = $('#textbox').val().length;
   if (formVal < 1) {
-    alert('No Data Supplied');
+    new Noty({text: 'No Data Supplied!',
+     type: 'error',
+     theme: 'mint',
+      layout: 'topCenter',
+     closeWith   : ['click'],
+     progressBar : true,
+     timeout     : 10000,
+     animation   : {
+      open  : 'animated bounceInDown',
+      close : 'animated bounceOutUp',
+      easing: 'swing',
+    }
+    }).show();
     console.log('Form data length is ' + formVal);
   } else {
     loader()
